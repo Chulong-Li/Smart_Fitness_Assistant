68b918fd5b8e95324f97460bc65da2af09ae381d
Chulong Li <li.7003@osu.edu>
2019-05-29 15:50:15 -0400

    Add UI framework

--- app/src/main/java/com/example/smart_fitness/MainActivity.java
+++ app/src/main/java/com/example/smart_fitness/MainActivity.java
@@ -1,42 +1,153 @@
 package com.example.smart_fitness;
 
-import android.os.Bundle;
-import android.support.design.widget.BottomNavigationView;
+
 import android.support.v7.app.AppCompatActivity;
-import android.support.annotation.NonNull;
-import android.view.MenuItem;
-import android.widget.TextView;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
-    private TextView mTextMessage;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.scaledrone.lib.Listener;
+import com.scaledrone.lib.Member;
+import com.scaledrone.lib.Room;
+import com.scaledrone.lib.RoomListener;
+import com.scaledrone.lib.Scaledrone;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+import java.util.Random;
 
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
+public class MainActivity extends AppCompatActivity implements RoomListener {
+
+    // replace this with a real channelID from Scaledrone dashboard
+    private String channelID = "D5TG1Rdxqp0gtxQu";
+    private String roomName = "my-room";
+    private EditText editText;
+    private Scaledrone scaledrone;
+    private MessageAdapter messageAdapter;
+    private ListView messagesView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.message);
-        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        editText = (EditText) findViewById(R.id.editText);
+
+        messageAdapter = new MessageAdapter(this);
+        messagesView = (ListView) findViewById(R.id.messages_view);
+        messagesView.setAdapter(messageAdapter);
+
+        MemberData data = new MemberData(getRandomName(), getRandomColor());
+
+        scaledrone = new Scaledrone(channelID, data);
+        scaledrone.connect(new Listener() {
+            @Override
+            public void onOpen() {
+                System.out.println("Scaledrone connection open");
+                scaledrone.subscribe(roomName, MainActivity.this);
+            }
+
+            @Override
+            public void onOpenFailure(Exception ex) {
+                System.err.println(ex);
+            }
+
+            @Override
+            public void onFailure(Exception ex) {
+                System.err.println(ex);
+            }
+
+            @Override
+            public void onClosed(String reason) {
+                System.err.println(reason);
+            }
+        });
     }
 
+    public void sendMessage(View view) {
+        String message = editText.getText().toString();
+        if (message.length() > 0) {
+            scaledrone.publish(roomName, message);
+            editText.getText().clear();
+        }
+    }
+
+    @Override
+    public void onOpen(Room room) {
+        System.out.println("Conneted to room");
+    }
+
+    @Override
+    public void onOpenFailure(Room room, Exception ex) {
+        System.err.println(ex);
+    }
+
+    @Override
+    public void onMessage(Room room, com.scaledrone.lib.Message receivedMessage) {
+        final ObjectMapper mapper = new ObjectMapper();
+        try {
+            final MemberData data = mapper.treeToValue(receivedMessage.getMember().getClientData(), MemberData.class);
+            boolean belongsToCurrentUser = receivedMessage.getClientID().equals(scaledrone.getClientID());
+            final Message message = new Message(receivedMessage.getData().asText(), data, belongsToCurrentUser);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    messageAdapter.add(message);
+                    messagesView.setSelection(messagesView.getCount() - 1);
+                }
+            });
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getRandomName() {
+        String[] adjs = {"autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue", "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long", "late", "lingering", "bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering", "withered", "wild", "black", "young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine", "polished", "ancient", "purple", "lively", "nameless"};
+        String[] nouns = {"waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly", "feather", "grass", "haze", "mountain", "night", "pond", "darkness", "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder", "violet", "water", "wildflower", "wave", "water", "resonance", "sun", "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog", "smoke", "star"};
+        return (
+                adjs[(int) Math.floor(Math.random() * adjs.length)] +
+                        "_" +
+                        nouns[(int) Math.floor(Math.random() * nouns.length)]
+        );
+    }
+
+    private String getRandomColor() {
+        Random r = new Random();
+        StringBuffer sb = new StringBuffer("#");
+        while(sb.length() < 7){
+            sb.append(Integer.toHexString(r.nextInt()));
+        }
+        return sb.toString().substring(0, 7);
+    }
+}
+
+class MemberData {
+    private String name;
+    private String color;
+
+    public MemberData(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public MemberData() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        return "MemberData{" +
+                "name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
 }
---
+++ app/src/main/java/com/example/smart_fitness/Message.java
@@ -0,0 +1,25 @@
+package com.example.smart_fitness;
+
+public class Message {
+    private String text;
+    private MemberData memberData;
+    private boolean belongsToCurrentUser;
+
+    public Message(String text, MemberData data, boolean belongsToCurrentUser) {
+        this.text = text;
+        this.memberData = data;
+        this.belongsToCurrentUser = belongsToCurrentUser;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public MemberData getMemberData() {
+        return memberData;
+    }
+
+    public boolean isBelongsToCurrentUser() {
+        return belongsToCurrentUser;
+    }
+}
