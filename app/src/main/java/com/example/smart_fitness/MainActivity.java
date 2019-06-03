9e26960a5162407c04b0a5c67006583d142a5479
Chulong Li <li.7003@osu.edu>
2019-06-02 20:51:44 -0400

    Workable Chat UI

--- app/src/main/java/com/example/smart_fitness/MainActivity.java
+++ app/src/main/java/com/example/smart_fitness/MainActivity.java
@@ -6,90 +6,45 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.scaledrone.lib.Listener;
-import com.scaledrone.lib.Member;
-import com.scaledrone.lib.Room;
-import com.scaledrone.lib.RoomListener;
-import com.scaledrone.lib.Scaledrone;
 
-import java.util.Random;
+public class MainActivity extends AppCompatActivity{
 
-public class MainActivity extends AppCompatActivity implements RoomListener {
 
-    // replace this with a real channelID from Scaledrone dashboard
-    private String channelID = "fGneqoNRxJlK3M5a";
-    private String roomName = "observable-room";
     private EditText editText;
-    private Scaledrone scaledrone;
     private MessageAdapter messageAdapter;
     private ListView messagesView;
+
+    private MemberData data = new MemberData("Steve", "#bb0000");
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText = (EditText) findViewById(R.id.editText);
-
+        editText = findViewById(R.id.editText);
         messageAdapter = new MessageAdapter(this);
-        messagesView = (ListView) findViewById(R.id.messages_view);
+        messagesView = findViewById(R.id.messages_view);
         messagesView.setAdapter(messageAdapter);
 
-        MemberData data = new MemberData(getRandomName(), getRandomColor());
-
-        scaledrone = new Scaledrone(channelID, data);
-        scaledrone.connect(new Listener() {
+        final Message welcome = new Message("Hi, I'm Steve! What can I help you?", data, false);
+        runOnUiThread(new Runnable() {
             @Override
-            public void onOpen() {
-                System.out.println("Scaledrone connection open");
-                scaledrone.subscribe(roomName, MainActivity.this);
-            }
-
-            @Override
-            public void onOpenFailure(Exception ex) {
-                System.err.println(ex);
-            }
-
-            @Override
-            public void onFailure(Exception ex) {
-                System.err.println(ex);
-            }
-
-            @Override
-            public void onClosed(String reason) {
-                System.err.println(reason);
+            public void run() {
+                messageAdapter.add(welcome);
+                messagesView.setSelection(messagesView.getCount() - 1);
             }
         });
+
     }
 
     public void sendMessage(View view) {
-        String message = editText.getText().toString();
-        if (message.length() > 0) {
-            scaledrone.publish(roomName, message);
-            editText.getText().clear();
-        }
-    }
+        String text = editText.getText().toString();
+        if (text.length() > 0) {
 
-    @Override
-    public void onOpen(Room room) {
-        System.out.println("Conneted to room");
-    }
-
-    @Override
-    public void onOpenFailure(Room room, Exception ex) {
-        System.err.println(ex);
-    }
-
-    @Override
-    public void onMessage(Room room, com.scaledrone.lib.Message receivedMessage) {
-        final ObjectMapper mapper = new ObjectMapper();
-        try {
-            final MemberData data = mapper.treeToValue(receivedMessage.getMember().getClientData(), MemberData.class);
-            boolean belongsToCurrentUser = receivedMessage.getClientID().equals(scaledrone.getClientID());
-            final Message message = new Message(receivedMessage.getData().asText(), data, belongsToCurrentUser);
+            // We provide a question here!
+            final Message message = new Message(text, data, true);
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
@@ -97,28 +52,20 @@ public class MainActivity extends AppCompatActivity implements RoomListener {
                     messagesView.setSelection(messagesView.getCount() - 1);
                 }
             });
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-    }
+            editText.getText().clear();
 
-    private String getRandomName() {
-        String[] adjs = {"autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue", "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long", "late", "lingering", "bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering", "withered", "wild", "black", "young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine", "polished", "ancient", "purple", "lively", "nameless"};
-        String[] nouns = {"waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly", "feather", "grass", "haze", "mountain", "night", "pond", "darkness", "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder", "violet", "water", "wildflower", "wave", "water", "resonance", "sun", "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog", "smoke", "star"};
-        return (
-                adjs[(int) Math.floor(Math.random() * adjs.length)] +
-                        "_" +
-                        nouns[(int) Math.floor(Math.random() * nouns.length)]
-        );
-    }
 
-    private String getRandomColor() {
-        Random r = new Random();
-        StringBuffer sb = new StringBuffer("#");
-        while(sb.length() < 7){
-            sb.append(Integer.toHexString(r.nextInt()));
+
+            // Response by IBM Watson Assistant
+            final Message response = new Message("I can only say HEY, dude!", data, false);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    messageAdapter.add(response);
+                    messagesView.setSelection(messagesView.getCount() - 1);
+                }
+            });
         }
-        return sb.toString().substring(0, 7);
     }
 }
 
@@ -131,9 +78,6 @@ class MemberData {
         this.color = color;
     }
 
-    public MemberData() {
-    }
-
     public String getName() {
         return name;
     }
@@ -142,11 +86,4 @@ class MemberData {
         return color;
     }
 
-    @Override
-    public String toString() {
-        return "MemberData{" +
-                "name='" + name + '\'' +
-                ", color='" + color + '\'' +
-                '}';
-    }
 }
