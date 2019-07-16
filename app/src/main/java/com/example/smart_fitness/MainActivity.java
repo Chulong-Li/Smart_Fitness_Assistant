c673b3b1a942c1d0556b399bd61761eb742ef18c
qingdaoalex <tsingtaoalex@outlook.com>
2019-07-16 17:13:16 -0400

    fixed background shrinking when keyboard appears

--- app/src/main/java/com/example/smart_fitness/MainActivity.java
+++ app/src/main/java/com/example/smart_fitness/MainActivity.java
@@ -97,6 +97,7 @@ public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setBackgroundDrawableResource(R.drawable.background1);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
